0ea811e7fddc32b8368d88795ba4eeca61f7562a
haiduc100 <ducnvgch190188@fpt.edu.vn>
2021-11-02 14:34:04 +0700

    Update Detail.js

--- src/components/pages/DetailsPage/Detail.js
+++ src/components/pages/DetailsPage/Detail.js
@@ -1,45 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Detail.css";
 
-const Detail = (props) => {
-  const [selectItem, setSelectItem] = useState(null);
-
-  useEffect(() => {
-    setSelectItem(
-      props.heroes.find((hero) => parseInt(hero.id) === props.selectedItem)
-    );
-  }, [props]);
-
-  const changeName = (event) => {
-    var newHeroes = props.heroes;
-    var selected = newHeroes.find(
-      (hero) => parseInt(hero.id) === props.selectedItem
-    );
-    selected.name = event.target.value;
-    setSelectItem({ ...selectItem, name: event.target.value });
-    props.setHeroes(newHeroes);
-  };
-  if (selectItem !== null && selectItem !== undefined) {
-    return (
+const Detail = () => {
+  return (
+    <div>
+      <h2> Details</h2>
       <div>
-        <h2>{selectItem} Details</h2>
-        <div>
-          <span>id: {selectItem.id}</span>
-        </div>
-        <div>
-          <label htmlFor="hero-name">Hero name: </label>
-          <input
-            id="hero-name"
-            placeholder="name"
-            onChange={changeName}
-            value={selectItem}
-          />
-        </div>
+        <span>id: </span>
       </div>
-    );
-  } else {
-    return <div></div>;
-  }
+      <div>
+        <label htmlFor="hero-name">Hero name: </label>
+        <input id="hero-name" placeholder="name" />
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
